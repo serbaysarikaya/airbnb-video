648238f4979f8ae4aa9e6eef2cd22287ce231cad
serbaysarikaya <ssarikaya@ritma.com.tr>
2023-12-03 03:41:12 +0300

    Categories UI created

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,6 +7,8 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "./Categories";
+
 import React from "react";
 import { SafeUser } from "@/app/types";
 
@@ -46,6 +48,7 @@ const Navbar: React.FC<NavbarProps> = ({
         </div>
       </Container>
     </div>
+    <Categories/>
    
   </div>
   );
